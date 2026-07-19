56748db77f8cafd9e5e8bd136be5a8dcb6e4855d
SwapnilGhodekar <swapnilghodekar0123@gmail.com>
2026-07-19 17:03:28 +0530

    InOrderTraversal

---
+++ Tree/InOrderTree.java
@@ -0,0 +1,43 @@
+
+class Node{
+    int val;
+    Node left;
+    Node right;
+    
+    Node(int val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+    
+    static int indx = -1;
+    static Node binaryTree(int [] preOrder){
+        indx++;
+        if(preOrder[indx] == -1){
+            return null;
+        }
+        
+        Node root = new Node(preOrder[indx]);
+        root.left = binaryTree(preOrder);
+        root.right = binaryTree(preOrder);
+        
+        return root;
+    }
+    
+      public static void inOrder(Node root){
+        if(root == null){
+            return ;
+        }
+         inOrder(root.left);
+         System.out.print(root.val+" ");
+         inOrder(root.right);
+         
+    }
+}
+class InOrderTree {
+    public static void main(String[] args){
+        int [] preOrder = {5, 3, 2, -1, -1, 4, -1, -1, 7, -1, 8, -1, -1};
+        Node root = Node.binaryTree(preOrder);
+        Node.inOrder(root);
+    }
+}
